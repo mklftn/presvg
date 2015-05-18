ece17d59f899fb9def363e297b3737df93481372
mklftn <mklftn@gmail.com>
2015-05-18 12:43:36 +0200

    Validation server side

--- client/template/emailForm.js
+++ client/template/emailForm.js
@@ -1,21 +1,40 @@
 Template.emailForm.events({
 	'click .email_send': function () {
-		var decorator = document.getElementById('decorator');
-		var input = document.getElementById('input');
-		decorator.isInvalid = !input.validity.valid;
-		if(input.validity.valid){
-			Meteor.call('insertEmail', input.value);
+		checkMail();
+	},
+	'keypress input': function (e, template) {
+		if (e.which === 13) {
+			checkMail();
 		}
 	}
 });
 
 Template.emailFormXS.events({
 	'click .email_send': function () {
-		var decorator = document.getElementById('decoratorXS');
-		var input = document.getElementById('inputXS');
-		decorator.isInvalid = !input.validity.valid;
-		if(input.validity.valid){
-			Meteor.call('insertEmail', input.value);
+		checkMailXS()
+	},
+	'keypress inputXS': function (e, template) {
+		if (e.which === 13) {
+			checkMailXS();
 		}
 	}
 });
+
+function checkMail () {
+	var decorator = document.getElementById('decorator');
+	var input = document.getElementById('input');
+	decorator.isInvalid = !input.validity.valid;
+	if(input.validity.valid){
+		Meteor.call('insertEmail', input.value);
+	}
+}
+
+function checkMailXS () {
+	var decorator = document.getElementById('decoratorXS');
+	var input = document.getElementById('inputXS');
+	decorator.isInvalid = !input.validity.valid;
+	if(input.validity.valid){
+		Meteor.call('insertEmail', input.value);
+	}
+}
+
