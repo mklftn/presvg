46198cb20da136e45f070d78ef6ccf158b73d1cd
mklftn <mklftn@gmail.com>
2015-05-18 12:21:55 +0200

    DAO email

--- client/template/emailForm.js
+++ client/template/emailForm.js
@@ -1,15 +1,21 @@
 Template.emailForm.events({
 	'click .email_send': function () {
 		var decorator = document.getElementById('decorator');
-        var input = document.getElementById('input');
-        decorator.isInvalid = !input.validity.valid;
+		var input = document.getElementById('input');
+		decorator.isInvalid = !input.validity.valid;
+		if(input.validity.valid){
+			Meteor.call('insertEmail', input.value);
+		}
 	}
 });
 
 Template.emailFormXS.events({
 	'click .email_send': function () {
 		var decorator = document.getElementById('decoratorXS');
-        var input = document.getElementById('inputXS');
-        decorator.isInvalid = !input.validity.valid;
+		var input = document.getElementById('inputXS');
+		decorator.isInvalid = !input.validity.valid;
+		if(input.validity.valid){
+			Meteor.call('insertEmail', input.value);
+		}
 	}
 });
