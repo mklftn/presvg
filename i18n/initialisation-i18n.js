cccdeb887781cefa2ae1a18b92fea131d30860bb
mklftn <mklftn@gmail.com>
2015-06-13 22:41:50 +0200

    Langue = fr provisoire

--- i18n/initialisation-i18n.js
+++ i18n/initialisation-i18n.js
@@ -10,6 +10,10 @@ if (Meteor.isClient) {
   Meteor.startup(function () {
     Session.set("showLoadingIndicator", true);
 
+    //Provisoire
+    var language = "fr";
+
+/*
     var language = getUserLanguage();
 
 
@@ -18,6 +22,7 @@ if (Meteor.isClient) {
     }else {
       language = "en"
     }
+*/
 
     TAPi18n.setLanguage(language)
     .done(function () {
