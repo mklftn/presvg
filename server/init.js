3466a53bf9e822cd9989388be1dcbfc783a7992a
Mickael Lecoq <m.blacksad@gmail.com>
2015-07-02 00:19:23 +0200

    passage sur prerender

--- server/init.js
+++ server/init.js
@@ -1,5 +1,9 @@
 // Permet d'ajouter les attributs de page pour utiliser polymer
 Meteor.startup(function() {
+
+   prerenderio.set('prerenderToken', 'LCt2ABpRIeH9eH1Xfqf'));
+
+   	  
   return Inject.rawModHtml('addUnresolved', function(html) {
     return html = html.replace('<body>', '<body unresolved fit layout vertical>');
   });
